8d859442c9852b6db4407db7b61f795eab86e46a
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-11-26 03:38:04 -0600

    Add initial implementation of adapters module with GmailEmailAdapter

---
+++ adapters/__init__.py
@@ -0,0 +1,12 @@
+# adapters/__init__.py
+from .EmailAdapter import GmailEmailAdapter
+# from .TelegramAdapter import TelegramAdapter
+# from .DiscordAdapter import DiscordAdapter
+
+__all__ = ["ADAPTERS"]
+
+ADAPTERS = {
+    "email": GmailEmailAdapter(),
+    # "telegram": TelegramAdapter(),
+    # "discord": DiscordAdapter(),
+}
