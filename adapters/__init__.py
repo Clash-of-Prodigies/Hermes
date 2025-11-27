10f18093d87083122c8a50663054cb63c527d55d
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-11-26 20:20:38 -0600

    Uncomment TelegramAdapter in adapters module to enable telegram support

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,12 +1,12 @@
 # adapters/__init__.py
 from .EmailAdapter import GmailEmailAdapter
-# from .TelegramAdapter import TelegramAdapter
+from .TelegramAdapter import TelegramAdapter
 # from .DiscordAdapter import DiscordAdapter
 
 __all__ = ["ADAPTERS"]
 
 ADAPTERS = {
     "email": GmailEmailAdapter(),
-    # "telegram": TelegramAdapter(),
+    "telegram": TelegramAdapter(),
     # "discord": DiscordAdapter(),
 }
