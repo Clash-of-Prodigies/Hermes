d54359ebd0f1dbe66efe20389829bfcba736a698
HeIsOdin <benjaminadedowole@gmail.com>
2025-12-21 12:16:00 -0600

    Change server port from 8000 to 5000 and disable debug mode

--- hermes.py
+++ hermes.py
@@ -77,4 +77,4 @@ def enqueue_message(payload: oreiades.MessageCreate):
     return jsonify(resp), 202
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
